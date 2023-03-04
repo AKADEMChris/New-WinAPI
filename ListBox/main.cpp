352e039a2e5cb46cd060fbf2d5b499e3e29384ec
Civov_wr34 <Civov_wr34@top-academy.ru>
2023-03-04 10:38:03 +0300

    Added ListBox

---
+++ ListBox/main.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include <cstdio>
+#include"resource.h"
+
+CONST CHAR* g_sz_STRING[] = { "This","is","My","First","List","Box" };
+//HWND hList;
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hList;
+		hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(g_sz_STRING) / sizeof(g_sz_STRING[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_sz_STRING[i]);
+		}
+	}
+	break;
+	case WM_COMMAND:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		CHAR sz_message[SIZE] = {};
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			sprintf(sz_message, "Вы выбрали пункт №%i, со значением %s", i, sz_buffer);
+			MessageBox(hwnd, sz_message, "Info", 0);
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	}
+	case WM_CLOSE: EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
